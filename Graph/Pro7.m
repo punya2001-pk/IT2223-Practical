0413e1884c9912b2715d9e574a96dcd2b2bf9e98
Punya Udayasiri <dka.punya@gmail.com>
2025-06-17 13:49:37 +0530

    Update Pro7.m

--- Graph/Pro7.m
+++ Graph/Pro7.m
@@ -1,9 +1,29 @@
-s=[1 1 1 1 1 2 2 2 2 3 3 3 3 4 4 4 4 5 5 5 6  8 9 10 7];
-t=[6 2 11 10 5 6 11 7 3 7 11 4 8  8 11 5 9 9 10 11 11 11 11 11 11];
-G=graph(s,t);
-h=plot(G,'LineWidth',2.5);
-highlight(h,[1:11],'NodeColor','blue');
-highlight(h,11,1:10,'EdgeColor','magenta');
-highlight(h, [1 1 2 3 4], [2 5 3 4 5], 'EdgeColor', 'green');
-highlight(h,[1 6 2 7 3 8 4 9 5 10],[6 2 7 3 8 4 9 5 10 1],'EdgeColor', 'red');
 
+s = [1 2 3 4 5 6 7 8 9 10 1 1 6 2 7 3 8 4 9 5 3 4 5 1 2];
+t = [11 11 11 11 11 11 11 11 11 11 10 6 2 7 3 8 4 9 5 10 4 5 1 2 3];
+G = graph(s, t);
+h = plot(G, 'LineWidth', 2.5);
+
+% Highlight all nodes 1 to 11 in blue
+for i = 1:11
+    highlight(h, i, 'NodeColor', 'blue');
+end
+
+% Highlight edges from node 11 to nodes 1 to 10 in magenta
+for i = 1:10
+    highlight(h, 11, i, 'EdgeColor', 'magenta');
+end
+
+% Highlight green edges
+green_s = [1 1 2 3 4];
+green_t = [2 5 3 4 5];
+for i = 1:length(green_s)
+    highlight(h, green_s(i), green_t(i), 'EdgeColor', 'green');
+end
+
+% Highlight red edges
+red_s = [1 6 2 7 3 8 4 9 5 10];
+red_t = [6 2 7 3 8 4 9 5 10 1];
+for i = 1:length(red_s)
+    highlight(h, red_s(i), red_t(i), 'EdgeColor', 'red');
+end
